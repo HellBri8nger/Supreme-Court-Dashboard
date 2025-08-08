a8f603eeb735ae0e279f60ebec556beb98adad1a
Rikki <60448443+HellBri8nger@users.noreply.github.com>
2025-08-08 11:44:54 +0530

    initial commit generated all of the files

--- Frontend/index.js
+++
@@ -1,9 +0,0 @@
-window.onload = PopulateFields
-
-function PopulateFields() {
-  let caseType = document.getElementById("CaseType");
-  let option = document.createElement("Option")
-
-  //TODO Once the API is implemented fetch data for options and parse them into html tags
-
-}
